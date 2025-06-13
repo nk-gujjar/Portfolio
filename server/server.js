ec1a8e9143e7a3daca9cc2a4fd07ab01e59fb0be
Nitesh kumar <115578281+nk-gujjar@users.noreply.github.com>
2025-06-13 12:29:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -72,16 +72,11 @@ app.post('/api/contact', async (req, res) => {
   }
 });
 
-// const path = require('path');
 
-// // Serve static files from React
-// app.use(express.static(path.join(__dirname, '../client/build')));
-
-// // Serve index.html on all unknown routes
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
-
+// Root route for test
+app.get('/', (req, res) => {
+  res.send('✅ Backend server is working!');
+});
 
 // Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
